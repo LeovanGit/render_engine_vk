57a1259bfc46cd4794a42cb70e50c91f8d071ea9
LeovanGit <vova11072003@gmail.com>
2026-08-20 16:31:53 +0300

    Request debug console and redirect stdout and stderr to it (for fmt and validation layers output)

--- render_engine_vk/src/main.cpp
+++ render_engine_vk/src/main.cpp
@@ -2,8 +2,24 @@
 
 #include <SDL.h>
 
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+void CreateDebugConsole()
+{
+	AllocConsole();
+
+	FILE* dummy = nullptr;
+	freopen_s(&dummy, "CONOUT$", "w", stdout);
+	freopen_s(&dummy, "CONOUT$", "w", stderr);
+}
+
 int main(int argc, char* argv[])
 {
+    #ifdef _DEBUG
+	    CreateDebugConsole();
+    #endif
+
 	RenderEngine::Init();
 	RenderEngine& renderEngine = RenderEngine::Get();
 
--- render_engine_vk/src/vk_helpers.cpp
+++ render_engine_vk/src/vk_helpers.cpp
@@ -99,21 +99,21 @@ VkSubmitInfo2 vk_helpers::SubmitInfo(
 	VkCommandBufferSubmitInfo* cmd,
 	VkSemaphoreSubmitInfo* signalSemaphoreInfo,
 	VkSemaphoreSubmitInfo* waitSemaphoreInfo)
-	{
-		VkSubmitInfo2 info = {};
-		info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO_2;
-		info.pNext = nullptr;
+{
+	VkSubmitInfo2 info = {};
+	info.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO_2;
+	info.pNext = nullptr;
 
-		info.waitSemaphoreInfoCount = waitSemaphoreInfo == nullptr ? 0 : 1;
-		info.pWaitSemaphoreInfos = waitSemaphoreInfo;
+	info.waitSemaphoreInfoCount = waitSemaphoreInfo == nullptr ? 0 : 1;
+	info.pWaitSemaphoreInfos = waitSemaphoreInfo;
 
-		info.signalSemaphoreInfoCount = signalSemaphoreInfo == nullptr ? 0 : 1;
-		info.pSignalSemaphoreInfos = signalSemaphoreInfo;
+	info.signalSemaphoreInfoCount = signalSemaphoreInfo == nullptr ? 0 : 1;
+	info.pSignalSemaphoreInfos = signalSemaphoreInfo;
 
-		info.commandBufferInfoCount = 1;
-		info.pCommandBufferInfos = cmd;
+	info.commandBufferInfoCount = 1;
+	info.pCommandBufferInfos = cmd;
 
-		return info;
+	return info;
 }
 
 bool vk_helpers::LoadShaderModule(
